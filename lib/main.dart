aa792719caca5d911af4a691442b8d41e7750d7e
Nipuni Fernando <nipunikfernando@gmail.com>
2024-12-02 17:10:14 +0530

    navigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firstt_app/model/category.dart';
 import 'package:firstt_app/view/LoginScreen.dart';
+import 'package:firstt_app/view/main_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() async {
--- lib/view/main_screen.dart
+++ lib/view/main_screen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import '../view/explore_screen.dart';
+//import 'package:airbnb_app_ui/view/message.dart';
+//import 'package:airbnb_app_ui/view/profile_page.dart';
+//import 'package:airbnb_app_ui/view/wishlists.dart';
 
 class AppMainScreen extends StatefulWidget {
   const AppMainScreen({super.key});
@@ -8,8 +12,84 @@ class AppMainScreen extends StatefulWidget {
 }
 
 class _AppMainScreenState extends State<AppMainScreen> {
+  int selectedIndex = 0;
+  late final List<Widget> page;
+
+   @override
+  void initState() {
+    page = [
+      const ExploreScreen(),
+      //const Wishlists(),
+      const Scaffold(),
+      //const MessagesScreen(),
+      //const ProfilePage(),
+    ];
+    super.initState();
+  }
+
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Colors.white,
+        elevation: 5,
+        iconSize: 32,
+        selectedItemColor: Colors.pinkAccent,
+        unselectedItemColor: Colors.black45,
+        selectedLabelStyle: const TextStyle(fontWeight: FontWeight.bold),
+        unselectedLabelStyle: const TextStyle(fontWeight: FontWeight.bold),
+        type: BottomNavigationBarType.fixed,
+        currentIndex: selectedIndex,
+        onTap: (index) {
+          setState(() {
+            selectedIndex = index;
+          });
+        },
+        items: [
+          BottomNavigationBarItem(
+            icon: Image.network(
+              "https://cdn3.iconfinder.com/data/icons/feather-5/24/search-512.png",
+              height: 30,
+              color: selectedIndex == 0 ? Colors.pinkAccent : Colors.black45,
+            ),
+            label: "Explore",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.favorite_border,
+              color: selectedIndex == 1 ? Colors.pinkAccent : Colors.black45,
+            ),
+            label: "Wishlists",
+          ),
+          BottomNavigationBarItem(
+            icon: Image.network(
+              "https://cdn-icons-png.flaticon.com/512/2111/2111307.png",
+              height: 30,
+              color: selectedIndex == 2 ? Colors.pinkAccent : Colors.black45,
+            ),
+            label: "Trip",
+          ),
+          BottomNavigationBarItem(
+            icon: Image.network(
+              "https://static.vecteezy.com/system/resources/thumbnails/014/441/006/small_2x/chat-message-thin-line-icon-social-icon-set-png.png",
+              height: 30,
+              color: selectedIndex == 3 ? Colors.pinkAccent : Colors.black45,
+            ),
+            label: "Message",
+          ),
+          BottomNavigationBarItem(
+            icon: Image.network(
+              "https://cdn-icons-png.flaticon.com/512/1144/1144760.png",
+              height: 30,
+              color: selectedIndex == 4 ? Colors.pinkAccent : Colors.black45,
+            ),
+            label: "Profile",
+          ),
+        ],
+      ),
+      body: page[selectedIndex],
+    );
   }
 }
